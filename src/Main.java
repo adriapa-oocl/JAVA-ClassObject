b2f815d432771b1b2c9377fe57fa4e92443b5232
ADRIAPA <ADRIAPA@OOCL.COM>
2021-07-27 14:39:10 +0800

    Removed displaying of attributes of Student/Added student two

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,22 @@
 import model.Student;
 
-import java.lang.reflect.Field;
-
 public class Main {
     public static void main(String[] args) {
         //Creating objects using the constructor for Student//
-        Student student = new Student("Patrick Cedie Adriano", 27, "Male", 001);
+        Student studentOne = new Student("Patrick Cedie Adriano", 27, "Male", 001);
+        Student studentTwo = new Student("Jesse Bryan Dy", 24, "Male", 002);
 
         //Invoking the objects created to the methods created in Student model//
-        student.setStudentName(student.studentName);
-        student.setStudentAge(student.studentAge);
-        student.setStudentGender(student.studentGender);
-        student.setStudentIdNum(student.studentIdNum);
-        student.printStudent();
+        studentOne.setStudentName(studentOne.studentName);
+        studentOne.setStudentAge(studentOne.studentAge);
+        studentOne.setStudentGender(studentOne.studentGender);
+        studentOne.setStudentIdNum(studentOne.studentIdNum);
+        studentTwo.setStudentName(studentTwo.studentName);
+        studentTwo.setStudentAge(studentTwo.studentAge);
+        studentTwo.setStudentGender(studentTwo.studentGender);
+        studentTwo.setStudentIdNum(studentTwo.studentIdNum);
 
-        //Display the attributes of Student model
-        System.out.println("\nStudent Attribute Names:");
-        for (Field attributeName: Student.class.getDeclaredFields())
-        {
-            System.out.println(attributeName.getName());
-        }
+        studentOne.printStudent();
+        studentTwo.printStudent();
     }
 }
